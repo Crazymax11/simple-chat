76a84754ffb82ed225498f831e95d259ad41a78f
GlebkaF <glaaab@mail.ru>
2016-03-21 18:15:23 +0600

    Messages storage first prototype

---
+++ messagesStorage.js
@@ -0,0 +1,37 @@
+/**
+ * Created by Глеб on 21.03.2016.
+ */
+"use strict";
+
+/**
+*   messageObject = {
+*            "from" : from,
+*            "text" : text,
+*            "time" : new Date()
+*        }
+**/
+
+class messagesStorage{
+    constructor(values) {
+        this.logger = logger.getLogger("messagesStorage");
+        this.logger.info("messagesModel started");
+        this.messages = [];
+    }
+
+    pushMessage(from, text){
+        var message = {
+            "from" : from,
+            "text" : text,
+            "time" : new Date()
+        };
+        this.messages.push(message)
+    }
+
+    getLasts(number){
+        number = number || 10;
+        return this.messages.length > number ? this.messages.slice(-number) : this.messages;
+    }
+}
+
+module.exports = messagesStorage;
+

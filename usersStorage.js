c602cf60d0d2bc76c6fc96c0654ea3e92b4171e7
Maxim Crazymax Sosnov <crazymax101@gmail.com>
2016-03-25 00:38:31 +0700

    usersStorage, минимально протестировано https://github.com/Crazymax11/simple-chat/issues/20

--- usersStorage.js
+++ usersStorage.js
@@ -14,89 +14,115 @@ class UsersStorage{
   constructor(values){
     this.dir = values.dir;
     this.defaultAvatarUrl = values.defaultAvatarUrl;
-    this.logger = values.logger("user storage");
-    fs.readdir( this.dir, (err, files) => {
-      if (err) throw (err);
-      this.keys = files;
-      this.logger.info("user storage loaded with %d users", this.keys.length);
-    });
+    this.logger = values.logger.getLogger("user storage");
+    this.logger.info("user storage loaded");
   }
   //callback (err, userInfo);
   createUser(values, callback){
     new Promise( (resolve, reject) => {
+      console.log("deb");
       // все ли данные представлены
       let notpresented = [];
-      if (!values.username) errstr.push("username");
-      if (!values.password) errstr.push("password");
-      if (!values.login)    errstr.push("login");
+      if (!values.username) notpresented.push("username");
+      if (!values.password) notpresented.push("password");
+      if (!values.login)    notpresented.push("login");
 
       if (notpresented.length) {
         let errstr = "provide all data to create a new user, you didn't provide these values: ";
         for(let v of notpresented) errstr += v + " ";
-        reject(new Error(errstr));}
+        reject(new Error(errstr));
       }
-
       values.avatar = values.avatar || this.defaultAvatarUrl;
-      if (this.keys[values.login]) reject(new Error("login already exists"))
       let obj = {
         login: values.login,
         password: values.password,
         avatar: values.avatar,
         username: values.username
       }
+      resolve(obj);
+    })
+    .then( (obj) => {return new Promise( (resolve, reject) => {
+      //проверим существует ли такой юзер
+      this._doesLoginExists({login: obj.login}, (ex) => ex? reject(new Error("such login already created")) : resolve(obj));
+    })})
+    .then( (obj) => {return new Promise( (resolve, reject) => {
+      //проверим занят ли юзернейм
+      this._doesUsernameExists({username: obj.username}, (ex) => ex? reject(new Error("such username already created")) : resolve(obj));
+    })})
+    .then( (obj) => {return new Promise( (resolve, reject) => {
+      //ну можно и создать юзера
       fs.writeFile(this.dir + '/' + obj.login, JSON.stringify(obj), (err) =>{
         if (err) reject(err);
         resolve(obj);
       })
-    })
-    .then(  (obj) => {
-      this.keys.push(obj.login);
-      callback(NULL, obj);
-    })
-    .catch( (err) => callback(err, NULL));
+    })})
+    .then(  (obj) => callback({}, obj))
+    .catch( (err) => callback(err, {}));
   }
   // callback( err, userInfo)
   readUser(values, callback){
-    new Promise( (resolve, reject) => {
-      if (!(values.login in this.keys)) reject(new Error("no such login"));
       fs.readFile(this.dir + '/' + values.login, (err, data) =>{
-        err? reject(err): resolve(json.parse(data));
-      })
-      .then( (data) => callback(NULL, data))
-      .catch( (err) => callback(err, NULL));
-    }
-  )
+        err ? callback(err, {}) : callback(err, JSON.parse(data));
+      });
+  }
   // обновляет данные о юзере, callback (err, userInfo)
   updateUser(values, callback){
     new Promise( (resolve, reject) => {
-      this.readUser(values, {err, obj} =>{
+      this.readUser(values, (err, obj) =>{
         if (err) reject(err);
-        if ("avatar" in values) obj.avatar = values.avatar;
-        if ("username" in values) obj.username = values.username;
-        if ("password" in values) obj.password = values.password;
+        if (values.avatar) obj.avatar = values.avatar;
+        if (values.username) obj.username = values.username;
+        if (values.password) obj.password = values.password;
         fs.writeFile(this.dir + '/' + values.login, JSON.stringify(obj), (err) => {
           if (err) reject(err);
           resolve(obj);
-        })
-      })
+        });
+      });
     })
-    .then(  (obj) => callback(NULL, obj))
-    .catch( (err) => callback(err, NULL));
+    .then(  (obj) => callback(obj, {}))
+    .catch( (err) => callback(err, {}));
   }
   //callback(err)
   deleteUser(values, callback){
     new Promise( (resolve, reject) => {
-      if (!(values.login in this.keys)) reject(new Error("no such login"));
-
       fs.unlink(this.dir + '/' + values.login, (err) =>{
         if (err) reject(err);
         resolve();
       })
     })
     .then( () => {
-      this.keys.splice(this.keys.indexOf(values.login), 1);
-      callback( NULL );
+      callback( {} );
     })
     .catch( (err) => callback(err) );
   }
+  _doesLoginExists(values, callback){
+      fs.exists(this.dir + "/" + values.login, callback);
+  }
+  _doesUsernameExists(values, callback){
+    new Promise ((resolve, reject) => {
+      fs.readdir( this.dir, (err, files) => {
+        err? reject(err) : resolve(files);
+      });
+    })
+    .then( (files) => {
+      if(!files.length) callback(false);
+      let promises = [];
+      for(var filename of files){
+        promises.push( new Promise( (resolve, reject) => {
+          fs.readFile(this.dir + "/" + filename, (err, data) => {
+            if (data) {
+              data = JSON.parse(data);
+              data.username === values.username ? reject(data) : resolve();
+            }
+          })
+        }))
+        Promise.all(promises)
+        .then( () => callback(false))
+        .catch ( (data) => callback(true));
+      }
+    })
+  }
 }
+
+
+module.exports = UsersStorage;
